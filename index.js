46e9e01aac8e6e40fe649f4eac8b3ecc093d0f1d
Steve Thompson <writetome51@gmail.com>
2020-08-27 14:33:20 -0600

    Function no longer checks data types of arguments

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-import {errorIfNotInteger} from 'error-if-not-integer';
-
-
 // index can be negative or positive.
 
 export function indexIsValid(index, arrayLength) {
-
-	errorIfNotInteger(index);
-	errorIfNotInteger(arrayLength);
 	if (arrayLength < 1) throw new Error('Input must be an integer greater than zero');
 
 	if (index < 0) index = (-index) - 1;
--- tests.js
+++ tests.js
@@ -3,38 +3,48 @@ if (indexIsValid(0, 1))
     console.log('test 1 passed');
 else
     console.log('test 1 FAILED');
+
 if (!(indexIsValid(1, 1)))
     console.log('test 2 passed');
 else
     console.log('test 2 FAILED');
+
+
 if (indexIsValid(-1, 1))
     console.log('test 3 passed');
 else
     console.log('test 3 FAILED');
+
 if (!(indexIsValid(-2, 1)))
     console.log('test 4 passed');
 else
     console.log('test 4 FAILED');
+
 if (indexIsValid(-2, 2))
     console.log('test 5 passed');
 else
     console.log('test 5 FAILED');
+
 if (indexIsValid(-1, 2))
     console.log('test 6 passed');
 else
     console.log('test 6 FAILED');
+
 if (indexIsValid(0, 2))
     console.log('test 7 passed');
 else
     console.log('test 7 FAILED');
+
 if (!(indexIsValid(-6, 5)))
     console.log('test 8 passed');
 else
     console.log('test 8 FAILED');
+
 if (!(indexIsValid(5, 5)))
     console.log('test 9 passed');
 else
     console.log('test 9 FAILED');
+
 // Make sure it can't modify a number variable.
 let i = -2, len = 2;
 indexIsValid(i, len);
@@ -42,3 +52,9 @@ if (i === -2)
     console.log('test 10 passed');
 else
     console.log('test 10 FAILED');
+
+
+if (indexIsValid('-1', '2'))
+	console.log('test 11 passed');
+else
+	console.log('test 11 FAILED');
